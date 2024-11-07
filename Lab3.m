2a64f51ecad4233a7e835ff71c46da92b300e8be
Laurel OBrien <111764031+LaurelEOB@users.noreply.github.com>
2024-11-07 13:53:22 -0700

    Update Lab3.m

--- Lab3.m
+++ Lab3.m
@@ -1,3 +1,4 @@
+close all; clear; clc;
 NACA = '4415'; % INPUT NUMBER ONLY
 
 m = str2num(NACA(1))/100;
@@ -9,13 +10,18 @@ x = 0:0.01:c;
 
 % equations
 
-y_t = (t*c/0.2)*((0.2969*sqrt(x/c))-(0.126*(x/c))-(0.3516*((x/c).^2))+0.2843*((x/c).^3))-(0.1036*((x/c).^4));
+y_t = @(x) (t*c/0.2)*((0.2969*sqrt(x/c))-(0.126*(x/c))-(0.3516*((x/c).^2))+0.2843*((x/c).^3))-(0.1036*((x/c).^4));
+y_t1 = y_t(x);
 
 %y_c1 = @(x) m
 
-dy_c1dx = 2*m*(c*p-x)/c/(p^2);
-dy_c2dx = (2*m*(c*p-x)) / (c*((1-p)^2));
+dy_c1dx = @(x) 2*m*(c*p-x)/c/(p^2);
+dy_c2dx = @(x) (2*m*(c*p-x)) / (c*((1-p)^2));
 
 
-zeta1 = atan(dy_c1dx);
-zeta2 = atan(dy_c2dx);
+zeta1 = @(x) atan(dy_c1dx(x));
+zeta2 = @(x) atan(dy_c2dx(x));
+
+
+x_u = @(x) x - y_t(x)*sin(zeta1);
+
