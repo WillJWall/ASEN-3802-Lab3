6232d472f96f6dc3cc2d21bec33341b750990aca
SirJuiceBox <123216149+SirJuiceBox@users.noreply.github.com>
2024-11-14 14:36:41 -0700

    ZERO LIFT ANGLE OF ATTACK INCLUDED ! - gabe_main.m

--- gabe_main.m
+++ gabe_main.m
@@ -1,62 +1,89 @@
-clc
-clear
-close all
-
-% inputs
+close all; clear; clc;
 NACA = '4415'; % INPUT NUMBER ONLY
 
-m = str2num(NACA(1))/100;
-p = str2num(NACA(2))/10;
-t = str2num(NACA(3))/10 + str2num(NACA(4))/100;
+% plotAirfoil('4415');
+% plotAirfoil('0012');
+plotAirfoil('0021');
+plotAirfoil('2421');
 
-c = 1;
-x = 0:0.01:c;
 
-% equations
-y_t = @(x) (t*c/0.2)*((0.2969*sqrt(x/c))-(0.126*(x/c))-(0.3516*((x/c).^2))+0.2843*((x/c).^3))-(0.1036*((x/c).^4));
-y_t1 = y_t(x);
-
-y_c1 = (2*p-(x/c)).*m.*x/(p^2);
-y_c2 = m.*(c-x).*(1+(x/c)-(2*p)) / ((1-p)^2);
-
-dy_c1dx = @(x) 2*m*(c*p-x)/c/(p^2); % 0<x<pc
-dy_c1dx1 = dy_c1dx(x);
-
-dy_c2dx = @(x) (2*m*(c*p-x)) / (c*((1-p)^2)); % pc<x<c
-dy_c2dx2 = dy_c2dx(x);
-
-% zeta definition
-% zeta1 = @(x) atan(dy_c1dx(x));
-zeta1 = atan(dy_c1dx1); % from 0<x<pc
-% zeta2 = @(x) atan(dy_c2dx(x));
-zeta2 = atan(dy_c2dx2); % from pc<x<c
-
-% index to switch from zeta1 to zeta2:
-p_switch = (length(x)-1) * p;
-
-for p = 1:length(x)
-    if p < p_switch
-        % upper coordinates
-        x_u(p) = x(p) - y_t(p) .* sin(zeta1(p));
-        y_u(p) = y_c1(p) + y_t(p) .* cos(zeta1(p));
-        
-        % lower coordinates
-        x_L(p) = x(p) + y_t(p) .* sin(zeta1(p));
-        y_L(p) = y_c1(p) - y_t(p) .* cos(zeta1(p));
-    else
-        % upper coordinates
-        x_u(p) = x(p) - y_t(p) .* sin(zeta2(p));
-        y_u(p) = y_c2(p) + y_t(p) .* cos(zeta2(p));
-        
-        % lower coordinates
-        x_L(p) = x(p) + y_t(p) .* sin(zeta2(p));
-        y_L(p) = y_c2(p) - y_t(p) .* cos(zeta2(p));
+function [a_L_0] = plotAirfoil(NACA)
+    % Clustering
+    angles = linspace(0,pi,101);
+    x = flip((cos(angles)+1)/2);
+    
+    % NACA Information
+    m = str2num(NACA(1))/100; % Max camber
+    p = str2num(NACA(2))/10; % Location of max camber
+    t = str2num(NACA(3))/10 + str2num(NACA(4))/100; % Max thickness
+    c = 1; % Chord Length
+    
+    % Half thickness
+    y_t = (t*c/0.2)*((0.2969*sqrt(x/c)) - (0.126*(x/c)) - (0.3516*((x/c).^2)) + (0.2843*((x/c).^3)) - (0.1036*((x/c).^4)) );
+    
+    % Mean Camber Line
+    if (p~=0) % For cambered airfoil
+        y_c1 = (2*p-(x/c)).*m.*x/(p^2);
+        y_c2 = m.*(c-x).*(1+(x/c)-(2*p)) / ((1-p)^2);
+    
+        dy_c1dx = 2*m*(c*p-x)/c/(p^2);
+        dy_c2dx = (2*m*(c*p-x)) / (c*((1-p)^2));
+    else % For symmetric airfoil
+        y_c1 = 0*x;
+        y_c2 = 0*x;
+    
+        dy_c1dx = 0*x;
+        dy_c2dx = 0*x;
     end
-end
+    
+    % Local angle
+    zeta1 = atan(dy_c1dx);
+    zeta2 = atan(dy_c2dx);
+    
+    % Lower Coordinates
+    x_ua =  x - (y_t.*sin(zeta1));
+    x_La =  x + (y_t.*sin(zeta1));
+    y_ua =  y_c1 + (y_t.*cos(zeta1));
+    y_La =  y_c1 - (y_t.*cos(zeta1));
+    
+    % Upper Coordinates
+    x_ub =  x - (y_t.*sin(zeta2));
+    x_Lb =  x + (y_t.*sin(zeta2));
+    y_ub =  y_c2 + (y_t.*cos(zeta2));
+    y_Lb =  y_c2 - (y_t.*cos(zeta2));
+    
+    midlimit = find(x>(p*c),1);
+    limit = find(x==c,1);
+    
+    % Final Coordinates
+    x_u = [x_ua(1,1:midlimit),x_ub(1,midlimit+1:limit)];
+    y_u = [y_ua(1,1:midlimit),y_ub(1,midlimit+1:limit)];
+    x_L = [x_La(1,1:midlimit),x_Lb(1,midlimit+1:limit)];
+    y_L = [y_La(1,1:midlimit),y_Lb(1,midlimit+1:limit)];
+    y_camber = [y_c1(1,1:midlimit),y_c2(1,midlimit+1:limit)];
 
-% plotting
-figure()
-hold on
-plot(x_u,y_u,'k') % upper
-plot(x_L,y_L,'b') % blue
-hold off
+    % Plotting
+    figure('Position', [40 60 700 400]); hold on; grid on;
+    title("Airfoil: NACA " + NACA)
+    xlabel("Distance along Chord");
+    ylabel("Thickness");
+    plot(x_u,y_u,'r.');
+    plot(x_L,y_L,'r.');
+    plot(x, y_camber,'r');
+    ylim([-0.2 0.3])
+    xlim([-0.001 1.001])
+
+    % zero lift angle of attack calculation
+    dy_camber = [dy_c1dx(1,1:midlimit),dy_c2dx(1,midlimit+1:limit)]; 
+    
+    y = linspace(0,pi,length(dy_camber));
+    figure()
+    plot(y,dy_camber);
+  
+    intdz = trapz(y,dy_camber.*(cos(y)-1));
+
+    a_L_0 = -1/pi * intdz;
+    a_L_0 = a_L_0 * 180/pi; % looking for - 2.2
+    disp("Zero Lift Angle of Attack for " + NACA)
+    disp(a_L_0)
+end
